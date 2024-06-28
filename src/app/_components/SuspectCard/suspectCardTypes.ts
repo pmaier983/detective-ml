0608fc1ea4630a177f995b3739003c08cf1e019b
Phillip ED Maier <pmaier983@gmail.com>
2024-06-28 09:00:54 -0400

    Delete src/app/_components/SuspectCard directory
    Dealing with annoying case problems

--- src/app/_components/SuspectCard/suspectCardTypes.ts
+++
@@ -1,7 +0,0 @@
-export const SUSPECT_CARD_VARIANTS = {
-  PREVIEW: "PREVIEW",
-  FULL: "FULL",
-} as const
-
-export type SuspectCardVariant =
-  (typeof SUSPECT_CARD_VARIANTS)[keyof typeof SUSPECT_CARD_VARIANTS]
